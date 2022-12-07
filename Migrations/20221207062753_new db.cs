754327ede5ec98ed0b441d38748262b3d0711022
Khalandar Bankapur <2000080648@hexaware.com>
2022-12-07 12:27:07 +0530

    first

---
+++ Migrations/20221207062753_new db.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LeaveApp.Migrations
+{
+    public partial class newdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Managers",
+                columns: table => new
+                {
+                    ManID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fname = table.Column<string>(nullable: false),
+                    Lname = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    ConPwd = table.Column<string>(nullable: false),
+                    Gen = table.Column<string>(nullable: false),
+                    Ph = table.Column<long>(nullable: false),
+                    Date = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Managers", x => x.ManID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmpID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fname = table.Column<string>(nullable: false),
+                    Lname = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    ConPwd = table.Column<string>(nullable: false),
+                    Gen = table.Column<string>(nullable: false),
+                    Ph = table.Column<long>(nullable: false),
+                    Date = table.Column<string>(nullable: false),
+                    ManID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmpID);
+                    table.ForeignKey(
+                        name: "FK_Employees_Managers_ManID",
+                        column: x => x.ManID,
+                        principalTable: "Managers",
+                        principalColumn: "ManID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Leaves",
+                columns: table => new
+                {
+                    LID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LStatus = table.Column<string>(nullable: false),
+                    LType = table.Column<string>(nullable: false),
+                    Sdate = table.Column<string>(nullable: false),
+                    Edate = table.Column<string>(nullable: false),
+                    EmpID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Leaves", x => x.LID);
+                    table.ForeignKey(
+                        name: "FK_Leaves_Employees_EmpID",
+                        column: x => x.EmpID,
+                        principalTable: "Employees",
+                        principalColumn: "EmpID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_ManID",
+                table: "Employees",
+                column: "ManID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Leaves_EmpID",
+                table: "Leaves",
+                column: "EmpID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Leaves");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Managers");
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveApp.Models
+{
+    public class Employee
+    {
+        [Required]
+        [Key]
+        [Display(Name = "EmployeeID")]
+        public int EmpID { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string Fname { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string Lname { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        public string ConPwd { get; set; }
+        [Required]
+        [Display(Name = "Gender")]
+        public string Gen { get; set; }
+        [Required]
+        [Display(Name = "Phone")]
+        public long Ph { get; set; }
+        [Required]
+        [Display(Name = "DOB")]
+        [DataType(DataType.Date)]
+        public string Date { get; set; }
+
+        [ForeignKey("ManID")]
+        public virtual Manager MID { get; set; }
+    }
+}
